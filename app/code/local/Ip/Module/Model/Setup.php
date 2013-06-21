2d8c965e8f8be8af18813853f610c8aad391fe15
ryan <ryan@inverseparadox.net>
2013-06-21 15:10:23 -0400

    Convert Indentation

--- app/code/local/Ip/Module/Model/Setup.php
+++ app/code/local/Ip/Module/Model/Setup.php
@@ -36,56 +36,56 @@
 class Ip_Module_Setup extends Mage_Eav_Model_Entity_Setup
 {
 
-    public function getDefaultEntities()
-    { 
-        return array(
-            'catalog_product' => array(
-                'entity_model'      => 'catalog/product',
-                'attribute_model'   => 'catalog/resource_eav_attribute',
-                'table'             => 'catalog/product',
-		        'additional_attribute_table' => 'catalog/eav_attribute',
-                'entity_attribute_collection' => 'catalog/product_attribute_collection',
-                'attributes'        => array(
-                    /*'ATTRIBUTE_CODE' => array(
-                        'attribute_set'     => 'Default',
-                        'group'             => 'General',
-                        'label'             => 'ATTRIBUTE_LABEL',
-                        'type'              => 'varchar',
+	public function getDefaultEntities()
+	{ 
+		return array(
+			'catalog_product' => array(
+				'entity_model'      => 'catalog/product',
+				'attribute_model'   => 'catalog/resource_eav_attribute',
+				'table'             => 'catalog/product',
+				'additional_attribute_table' => 'catalog/eav_attribute',
+				'entity_attribute_collection' => 'catalog/product_attribute_collection',
+				'attributes'        => array(
+					/*'ATTRIBUTE_CODE' => array(
+						'attribute_set'     => 'Default',
+						'group'             => 'General',
+						'label'             => 'ATTRIBUTE_LABEL',
+						'type'              => 'varchar',
 						'input'             => 'select',
-                        'default'           => '',
-                        'class'             => '',
-                        'backend'           => 'module/attribute_backend',
-                        'frontend'          => '',
-                        'source'            => 'module/source_data',                    
+						'default'           => '',
+						'class'             => '',
+						'backend'           => 'module/attribute_backend',
+						'frontend'          => '',
+						'source'            => 'module/source_data',                    
 						'global'            => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_GLOBAL,
-                        'visible'           => true,
-                        'required'          => false,
-                        'user_defined'      => false,
-                        'searchable'        => true,
-                        'filterable'        => true,
-                        'comparable'        => false,
-                        'visible_on_front'  => true,
-                        'visible_in_advanced_search' => false,
-                        'unique'            => false
-                    )*/
-               )
-           ),
+						'visible'           => true,
+						'required'          => false,
+						'user_defined'      => false,
+						'searchable'        => true,
+						'filterable'        => true,
+						'comparable'        => false,
+						'visible_on_front'  => true,
+						'visible_in_advanced_search' => false,
+						'unique'            => false
+					)*/
+			   )
+		   ),
 			'customer' => array(
 				'entity_model'      => 'customer/customer',
 				'attribute_model'   => 'customer/attribute',
 				'table'             => 'customer/entity',
 				'additional_attribute_table' => 'customer/eav_attribute',
-                'entity_attribute_collection' => 'customer/attribute_collection',
-                'attributes'        => array(
+				'entity_attribute_collection' => 'customer/attribute_collection',
+				'attributes'        => array(
 					/*'ATTRIBUTE_CODE' => array(
 						'label'             => 'ATTRIBUTE_LABEL',
-                        'type'              => 'varchar',
+						'type'              => 'varchar',
 						'input'             => 'text',
 						'default'           => '',
-                        'backend'           => '',
+						'backend'           => '',
 						'frontend'          => '',
 						'class'             => '',
-                        'source'            => '',                    
+						'source'            => '',                    
 						'global'            => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_GLOBAL,
 						'visible'           => true,
 						'required'          => false,
@@ -96,7 +96,7 @@ class Ip_Module_Setup extends Mage_Eav_Model_Entity_Setup
 						'visible_on_front'  => false,
 						'unique'            => false
 
-            		)*/
+					)*/
 				)
 			)
 		);
