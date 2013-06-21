496f90e9e5ddeafb216d8f0f491be1ee3408ba09
ryan <ryan@inverseparadox.net>
2013-06-21 15:11:52 -0400

    Convert Indentation

--- app/code/local/Ip/Module/Model/Attribute/Backend.php
+++ app/code/local/Ip/Module/Model/Attribute/Backend.php
@@ -3,12 +3,12 @@
 class Ip_Module_Model_Attribute_Backend extends Mage_Eav_Model_Entity_Attribute_Backend_Abstract 
 {
 	
-    public function beforeSave($object)
-    {		
+	public function beforeSave($object)
+	{		
 		// $attribute = $object->getData('ATTRIBUTE_CODE');
 		// do something to $attribute
 		// $object->setData('ATTRIBUTE_CODE', $attribute);
-        return $this;
-    }
+		return $this;
+	}
 	
 }
