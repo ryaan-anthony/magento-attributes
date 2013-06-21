9bf8778b473ae43029cbf8677d66f02c9a64ced3
ryan <ryan@inverseparadox.net>
2013-06-21 17:00:39 -0400

    Now supports adding editable fields for customer attributes to Magento Admin > Customer > Edit > Account Information

--- app/code/local/Ip/Module/sql/module_setup/mysql4-install-0.1.0.php
+++ app/code/local/Ip/Module/sql/module_setup/mysql4-install-0.1.0.php
@@ -1,3 +1,30 @@
 <?php
 	$installer = $this;
 	$installer->installEntities();
+
+	$installer->startSetup();
+
+	// add customer attributes to forms
+	$entity_type_id = $installer->getEntityTypeId('customer');
+	$table_name = $installer->getTable('customer/form_attribute');
+
+	$editable_attributes = array(
+		'ATTRIBUTE_CODE' => array(
+			'adminhtml_customer', // admin > customer > edit > "Account Information"
+			'customer_account_create', // (based on theme) frontend > register
+			'customer_account_edit' // (based on theme) frontend > customer > account > edit > "Account Information"
+		)
+	);
+
+	foreach($editable_attributes as $attribute_code => $forms){
+		$attribute = Mage::getModel('eav/entity_attribute')->loadByCode($entity_type_id, $attribute_code);
+		$attribute_id = $attribute->getId();
+		foreach($forms as $form){
+			$installer->run("
+			    INSERT INTO `{$table_name}` 
+			    VALUES ('{$form}','{$attribute_id}');
+			");
+		}
+	}
+
+	$installer->endSetup();
